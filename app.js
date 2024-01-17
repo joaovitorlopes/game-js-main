5d05fc2434f227ce98b7a15ded0fcd534e275e5c
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-17 18:37:12 -0400

    repetition structure

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 alert('Bem vindo ao jogo do número secreto');
 let secretNumber = 5;
-let selectedNumber = prompt('Escolha um número entre 1 e 10');
+let selectedNumber;
 
-// comparando o número secreto e o número escolhido
-if (selectedNumber == secretNumber) {
-    alert(`Parabéns, você acertou: ${secretNumber}`);
-} else {
-    if (selectedNumber > secretNumber) {
-        alert(`O número secreto é menor que ${selectedNumber}`);
+// enquanto não acertar
+while (selectedNumber != secretNumber) {
+    selectedNumber = prompt('Escolha um número entre 1 e 10');
+    // comparando o número secreto e o número escolhido
+    if (selectedNumber == secretNumber) {
+        alert(`Isso ai! Você descobriu o número secreto ${secretNumber}`);
     } else {
-        alert(`O número secreto é maior que ${selectedNumber}`);
+        if (selectedNumber > secretNumber) {
+            alert(`O número secreto é menor que ${selectedNumber}`);
+        } else {
+            alert(`O número secreto é maior que ${selectedNumber}`);
+        }
     }
 }
