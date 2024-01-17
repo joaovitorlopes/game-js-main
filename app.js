9e70041856a5cc9ee10fe570aa6ee0f70c49a769
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-17 17:48:38 -0400

    creating a condition

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ alert('Bem vindo ao jogo do número secreto');
 let secretNumber = 5;
 let selectedNumber = prompt('Escolha um número entre 1 e 10');
 
+// comparando o número secreto e o número escolhido
 if (selectedNumber == secretNumber) {
-    console.log(`Parabéns, você acertou: ${secretNumber}`);
+    alert(`Parabéns, você acertou: ${secretNumber}`);
+} else {
+    alert('Você errou!')
 }
