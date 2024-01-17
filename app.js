5eaf60a16a4355bc66019df36e4c7be0759ff041
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-17 18:43:11 -0400

    adding a counter

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ alert('Bem vindo ao jogo do número secreto');
 let secretNumber = 5;
 let selectedNumber;
 
+let counter = 1;
 // enquanto não acertar
 while (selectedNumber != secretNumber) {
     selectedNumber = prompt('Escolha um número entre 1 e 10');
     // comparando o número secreto e o número escolhido
     if (selectedNumber == secretNumber) {
-        alert(`Isso ai! Você descobriu o número secreto ${secretNumber}`);
+        alert(`Isso ai! Você descobriu o número secreto ${secretNumber} com ${counter} tentativas.`);
     } else {
         if (selectedNumber > secretNumber) {
             alert(`O número secreto é menor que ${selectedNumber}`);
         } else {
             alert(`O número secreto é maior que ${selectedNumber}`);
         }
+        counter++;
     }
 }
