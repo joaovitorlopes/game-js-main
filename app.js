e7b7a1f0ee6154a6a907fb81470dbfd4059dabeb
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-17 17:05:11 -0400

    comparing variables

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 alert('Bem vindo ao jogo do número secreto');
 let secretNumber = 5;
+let selectedNumber = prompt('Escolha um número entre 1 e 10');
 
-prompt('Escolha um número entre 1 e 10');
+if (selectedNumber === secretNumber) {
+    console.log(`Parabéns, você acertou: ${secretNumber}`);
+}
