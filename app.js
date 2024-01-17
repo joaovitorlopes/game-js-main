70da13ce54e6f26d99907eeb32670b7ce9105df2
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-17 17:13:32 -0400

    fixing bugs in if logic

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ alert('Bem vindo ao jogo do número secreto');
 let secretNumber = 5;
 let selectedNumber = prompt('Escolha um número entre 1 e 10');
 
-if (selectedNumber === secretNumber) {
+if (selectedNumber == secretNumber) {
     console.log(`Parabéns, você acertou: ${secretNumber}`);
 }
