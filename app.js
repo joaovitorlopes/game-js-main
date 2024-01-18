76b6d8221b61759df78bd1548d9cb91b885ec27a
joaovitorlopes <jvlaaa+github@gmail.com>
2024-01-18 11:28:53 -0400

    testing fixing the visibility bug of the page

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-alert('Bem vindo ao jogo do número secreto');
-let max = 1000;
-let secretNumber = parseInt(Math.random() * max + 1);
-let selectedNumber;
+var setDivVisibility = (value) => document.getElementById('containerPage').style.visibility = value;
 
-let counter = 1;
-// enquanto não acertar
-while (selectedNumber != secretNumber) {
-    selectedNumber = prompt(`Escolha um número entre 1 e ${max}`);
-    // comparando o número secreto e o número escolhido
-    if (selectedNumber == secretNumber) {
-        break;
-    } else {
-        if (selectedNumber > secretNumber) {
-            alert(`O número secreto é menor que ${selectedNumber}`);
+async function start() {
+    alert('Bem vindo ao jogo do número secreto');
+    let max = 1000;
+    // let secretNumber = parseInt(Math.random() * max + 1);
+    let secretNumber = 5;
+    let selectedNumber;
+
+    let counter = 1;
+    // enquanto não acertar
+    while (selectedNumber != secretNumber) {
+        selectedNumber = prompt(`Escolha um número entre 1 e ${max}`);
+        // comparando o número secreto e o número escolhido
+        if (selectedNumber == secretNumber) {
+            break;
         } else {
-            alert(`O número secreto é maior que ${selectedNumber}`);
+            if (selectedNumber > secretNumber) {
+                alert(`O número secreto é menor que ${selectedNumber}`);
+            } else {
+                alert(`O número secreto é maior que ${selectedNumber}`);
+            }
+            counter++;
         }
-        counter++;
     }
+
+    // Operador ternário
+    let wordCounter = counter > 1 ? 'tentativas' : 'tentativa';
+    setDivVisibility('visible');
+    alert(`Isso ai! Você descobriu o número secreto ${secretNumber} com ${counter} ${wordCounter}.`)
 }
 
-// Operador ternário
-let wordCounter = counter > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${secretNumber} com ${counter} ${wordCounter}.`);
+start();
